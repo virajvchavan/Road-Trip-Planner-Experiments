edf86d6f90b15f1ff173f7ccca0aa889c371d551
virajvchavan <viraj@joshsoftware.com>
2018-05-04 17:55:07 +0530

    styled login page

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -14,17 +14,121 @@ if (isset($login)) {
 }
 ?>
 
-<!-- login form box -->
-<form method="post" action="login.php" name="loginform">
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+        body{
+  font-family: 'Open Sans', sans-serif;
+  background:#3498db;
+  margin: 0 auto 0 auto;  
+  width:100%; 
+  text-align:center;
+  margin: 20px 0px 20px 0px;   
+}
 
-    <label for="login_input_username">Username</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
+p{
+  font-size:12px;
+  text-decoration: none;
+  color:#ffffff;
+}
 
-    <label for="login_input_password">Password</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+h1{
+  font-size:1.5em;
+  color:#525252;
+}
 
-    <input type="submit"  name="login" value="Log in" />
+.box{
+  background:white;
+  width:300px;
+  border-radius:6px;
+  margin: 0 auto 0 auto;
+  padding:0px 0px 70px 0px;
+  border: #2980b9 4px solid; 
+}
 
-</form>
+.email{
+  background:#ecf0f1;
+  border: #ccc 1px solid;
+  border-bottom: #ccc 2px solid;
+  padding: 8px;
+  width:250px;
+  color:#AAAAAA;
+  margin-top:10px;
+  font-size:1em;
+  border-radius:4px;
+}
 
-<a href="register.php">Register new account</a>
+.password{
+  border-radius:4px;
+  background:#ecf0f1;
+  border: #ccc 1px solid;
+  padding: 8px;
+  width:250px;
+  font-size:1em;
+}
+
+.btn{
+  background:#2ecc71;
+  width:125px;
+  padding-top:5px;
+  padding-bottom:5px;
+  color:white;
+  border-radius:4px;
+  border: #27ae60 1px solid;
+  
+  margin-top:20px;
+  margin-bottom:20px;
+  float:left;
+  margin-left:16px;
+  font-weight:800;
+  font-size:0.8em;
+}
+
+.btn:hover{
+  background:#2CC06B; 
+}
+
+#btn2{
+  float:left;
+  background:#3498db;
+  width:125px;  padding-top:5px;
+  padding-bottom:5px;
+  color:white;
+  border-radius:4px;
+  border: #2980b9 1px solid;
+  
+  margin-top:20px;
+  margin-bottom:20px;
+  margin-left:10px;
+  font-weight:800;
+  font-size:0.8em;
+}
+
+#btn2:hover{ 
+background:#3594D2; 
+}
+    </style>
+</head>
+<body>
+    <form method="post" action="login.php" name="loginform">
+        <div class="box">
+            <h1>Road Trip Planner</h1>
+
+            <input id='login_input_username' type='text' name="user_name" value="username" class="email" required />
+              
+            <input id='login_input_username' type="password" name="user_password" value="email" class="email" autocomplete="off" required />
+              
+            <input type='submit' name='login' class="btn" value="Login"> <!-- End Btn -->
+            <a class="btn" href="register.php">Register</a>
+        </div> <!-- End Box -->
+          
+    </form>
+      
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+</body>
+</html>
+
+
